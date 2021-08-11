5ad1136666d05cea308600de7a3eebd03084ec79
Maciek <gabmaciej@gmail.com>
2021-08-12 01:20:43 +0200

    Added meetings history in SettingsPage

--- lib/pages/SettingsPage.dart
+++ lib/pages/SettingsPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -23,11 +24,37 @@ class SettingsPage extends StatelessWidget {
                           // starsza na pewno działąca wersja
                           // auth.signOut();
                         },
-                        child: Text('SignOut'))
+                        child: Text('SignOut')),
+                    Text('Your meetings history:'),
+                    FutureBuilder(
+                      future: getMeetings(context),
+                      builder: (_, snapshot) {
+                        if(snapshot.hasData){
+                          return ListView.builder(
+                            shrinkWrap: true,
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (_,index) {
+                                return ListTile(
+                                  title: Text(snapshot.data[index]['title']),
+                                );
+                              });
+                        }
+                        else if (snapshot.hasError) {
+                          return Text("Couldn't load data: ${snapshot.error}");
+                        }
+                        else{
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },),
                   ]))));
 
     }
     else
       return Scaffold(body: Center(child: CircularProgressIndicator()));
+  }
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getMeetings(BuildContext context) async{
+    var querySnapshot =  await FirebaseFirestore.instance.collection('profiles').doc(Provider.of<UserDataModel>(context).uid).collection('pastMeetings').get();
+    return querySnapshot.docs;
   }
 }
