f51200c7ec1854c9725dceddbbdd38d63450a8de
Maciek <gabmaciej@gmail.com>
2021-08-14 23:43:26 +0200

    Added basic implementation of notification coming from Firebase

--- lib/wrappers/AuthWrapper.dart
+++ lib/wrappers/AuthWrapper.dart
@@ -1,14 +1,90 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:test_auth_with_rolebased_ui/pages/SignInPage.dart';
-import 'package:test_auth_with_rolebased_ui/services/DatabaseService.dart';
-import 'package:test_auth_with_rolebased_ui/models/UserDataModel.dart';
 import 'DbWrapper.dart';
 
-class AuthWrapper extends StatelessWidget {
+class AuthWrapper extends StatefulWidget {
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
   final auth = FirebaseAuth.instance;
+  FirebaseMessaging firebaseMessaging;
+  String notificationText;
+
+  @override
+  void initState() {
+    super.initState();
+    firebaseMessaging = FirebaseMessaging.instance;
+    firebaseMessaging.subscribeToTopic("messaging");
+    firebaseMessaging.getToken().then((value) {
+      print(value);
+    });
+    FirebaseMessaging.instance.getInitialMessage().then((value) {
+      if (value != null) {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Notification"),
+                content: Text(value.notification.body),
+                actions: [
+                  TextButton(
+                    child: Text("Ok"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              );
+            });
+      }
+    });
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      print("message recieved");
+      print(event.notification.body);
+      print(event.data.values);
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Notification"),
+              content: Text(event.notification.body),
+              actions: [
+                TextButton(
+                  child: Text("Ok"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage event) {
+      print('Message clicked!');
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Notification"),
+              content: Text(event.notification.body),
+              actions: [
+                TextButton(
+                  child: Text("Ok"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
