0e565d92ccabcd354af5d4d928c92a0dd8f282db
Maciek <gabmaciej@gmail.com>
2021-09-04 00:26:13 +0200

    AuthService refactor

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -3,56 +3,30 @@ import 'DatabaseService.dart';
 
 class AuthService {
   final FirebaseAuth _auth;
-
   AuthService(this._auth);
-
-  //zwraca User lub null
+  //returns User or null
   Stream<User> get authStateChanges => _auth.authStateChanges();
-
-  //sign in in anon
-  Future signInAnon() async {
-    try {
-      UserCredential userCredential = await _auth.signInAnonymously();
-      User user = userCredential.user;
-      return user;
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
-
 //sign in  with email & password
   Future<String> signIn({String email, String password}) async {
-    try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
-      return "signed-in";
+    try {await _auth.signInWithEmailAndPassword(email: email, password: password);return "signed-in";
     } on FirebaseAuthException catch (e) {
       print(e.message + ' with error code : ${e.code}');
       return e.code;
     }
   }
-
 //register with email, password <- for Firebase Authentication; name and surname <- for Firestore document
-  Future<String> signUp(
-      {String email, String password, String firstName, String lastName}) async {
+  Future<String> signUp({String email, String password, String firstName, String lastName}) async {
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      await DatabaseService().updateUserData(
-          firstName, lastName, 'user', email, result.user.uid);
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      await DatabaseService().updateUserData(firstName, lastName, 'user', email, result.user.uid);
       return "signed-up";
     } on FirebaseAuthException catch (e) {
       print(e.message + ' with error code : ${e.code}');
       return e.code;
     }
   }
-
 //sing out
   Future<void> singOut() async {
     await _auth.signOut();
-  }
-
-  Future<void> reloadData() async {
-    await _auth.currentUser.reload();
   }
 }
